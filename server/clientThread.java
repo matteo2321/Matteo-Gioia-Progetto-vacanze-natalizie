f0610fe7794eee94650f85fe58121ea7a1aa019c
gioiaabertin <118894495+gioiaabertin@users.noreply.github.com>
2023-01-02 14:49:24 +0100

    server protocollo aggiornamenti risposta
      //0/1;xr;yr;xp;yp
                   // risposta 0= risp normale solo coordinate
                   //risosta 1= risp evento coordinate racchettina(x;y) + pallina(x;y)

--- server/clientThread.java
+++ server/clientThread.java
@@ -18,42 +18,52 @@ public class clientThread extends Thread {
     @Override
     public void run() {
         risposta = new Byte[n];
-        int i = 0;
 
         shared inst = shared.getInstance();
         while (true) {
             try {
 
                 String[] msg = in.readLine().split(";");
-                if(campo[0]==1){
-
+                if (campo[0] == 0) {
+                    msg[0]=0;
+                    for (int i = 1; i < 3; i++) {
+                        risposta[i] = msg[i];
+                    }
                 }
-                for (String campo : msg) {
-                    if(campo){}
-                    
+                else if(campo[0]==1){
+                    msg[0] = 1;
+                    for (int i = 1; i < 5; i++) {
+                        risposta[i] = msg[i];
+                    }
                 }
-             /*    if (msg[0] == 1) {
-                    risposta[i] = msg[1];
-                    i++;
-                    risposta[i] = msg[2];
-                }
-                else if(msg[0] == 2)
-                {
-                    risposta[i] = msg[1];
-                    i++;
-                    risposta[i] = msg[2];
-                    i++;
-                    risposta[i] = msg[1];
-                    i++;
-                    risposta[i] = msg[2];
+                //0/1;xr;yr;xp;yp
+               // risposta 0= risp normale solo coordinate
+               //risosta 1= risp evento coordinate racchettina(x;y) + pallina(x;y)
 
+                /*
+                 * if (msg[0] == 1) {
+                 * risposta[i] = msg[1];
+                 * i++;
+                 * risposta[i] = msg[2];
+                 * }
+                 * else if(msg[0] == 2)
+                 * {
+                 * risposta[i] = msg[1];
+                 * i++;
+                 * risposta[i] = msg[2];
+                 * i++;
+                 * risposta[i] = msg[1];
+                 * i++;
+                 * risposta[i] = msg[2];
+                 * 
+                 * 
+                 * }
+                 */
 
-                }*/
-
-                System.out.println(_socket.id + " ha ricevuto : " + str);
-                MySocket destinatario = inst.findDifferentSocketById(_socket.id); //ritorna la socket dell'altro player
-                if (destinatario != null)
-                    destinatario.sendMessage(str); //richiamo il metodo di MySocket che scrive sul buffer 
+               // System.out.println(_socket.id + " ha ricevuto : " + str);
+                MySocket destinatario = inst.findDifferentSocketById(_socket.id); // ritorna la socket dell'altro player
+                if (destinatario != null/*&& velocita<100*/)
+                    destinatario.sendMessage(str); // richiamo il metodo di MySocket che scrive sul buffer
 
             } catch (IOException e) {
                 // connessione chiusa
