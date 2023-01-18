fead65b68d2845106dba830557c01c6c8a728d57
gioiaabertin <118894495+gioiaabertin@users.noreply.github.com>
2023-01-18 10:16:10 +0100

    correziuone

--- server/clientThread.java
+++ server/clientThread.java
@@ -56,7 +56,7 @@ public class clientThread extends Thread {
                         for (int i = 1; i < 5; i++) {
                             int foo = Integer.parseInt(campo[i]);
                             int temp = foo*(-1);
-                            if(contrllo(campo[1],campo[3],campo[2],campo[4],r1,r2/*,v */)==true)
+                            if(controllo(campo[1],campo[3],campo[2],campo[4],r1,r2/*,v */)==true)
                             r+=temp+";";
                           //  risposta = r.getBytes();
 
